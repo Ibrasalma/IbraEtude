9018795ef0da8ff90bed741ddc77399aecee471e
Ibrasalma <47360972+Ibrasalma@users.noreply.github.com>
2020-04-28 15:44:29 +0800

    Update 2020_03_18_135253_create_universities_table.php

--- database/migrations/2020_03_18_135253_create_universities_table.php
+++ database/migrations/2020_03_18_135253_create_universities_table.php
@@ -16,8 +16,8 @@ class CreateUniversitiesTable extends Migration
         Schema::create('universities', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
-            $table->string('ville');
-            $table->string('province');
+            $table->enum('ville',['Beijing','Cangzhou','ChangSha','Changchun','Changzhou','Chengdu','Chenzhou','Chongqing','Dalian','Foshan','Guilin','Hangzhou','Harbin','Hefei','Huaian','Huizhou','Huzhou','Jiangmen','Jiaxing','Jilin','Jinan','Jinhua','Jining','Kunming','Lanzhou','Lhasa','Linyi','Lishui','Nanchang','Nanjing','Nanning','Nantong','Nanyang','Ningbo','Qingdao','Sanya','Shanghai','Shantou','Shaoxing','Shenzhen','Suizhou','Suzhou, Anhui','Taian','Taiyuan','Taizhou','Tianjin','Tonghua','Wenzhou','Wuhan','Wuhu','Wuxi','Wuzhong','Xian','Xiamen','Xuzhou','Yangzhou','Yantai','Yencheng','Zhengzhou','Zhenjiang']);
+            $table->enum('province',['anhui','beijing','chongqing','fujian','gansu','guangdong','guangxi','guizhou','henan','hebei','hainan','heilongjiang','huan','hubei','jiangsu','jiangxi','jilin','liaoning','mongolie interieure','ningxia','qinghai','shanxi','shandong','shaanxi','sichuan','tibet','yunnan','xinjiang','zhejiang','zhilin']);
             $table->text('details');
             $table->string('slogan');
             $table->string('code');
